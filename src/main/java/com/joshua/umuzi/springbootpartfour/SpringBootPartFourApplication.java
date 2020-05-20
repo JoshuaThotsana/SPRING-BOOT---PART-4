d8810c5975dcb9b86ce27bd4e4295766772c47c3
Thotsana <thotsana.mabotsa@umuzi.org>
2020-05-20 17:46:00 +0200

    first commit

---
+++ src/main/java/com/joshua/umuzi/springbootpartfour/SpringBootPartFourApplication.java
@@ -0,0 +1,13 @@
+package com.joshua.umuzi.springbootpartfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPartFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPartFourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joshua/umuzi/springbootpartfour/SpringBootPartFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.umuzi.springbootpartfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPartFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
